2f989bb5f4e3cfcb9cffe8734f2af8570191fd35
Iarley Souza <167232053+IarleySouza@users.noreply.github.com>
2025-06-15 22:41:54 -0300

    add video

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,22 +1,21 @@
-import express, { Application } from 'express'
-import http from 'http'
-import { Server } from 'socket.io'
-import path from 'path';
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const path_1 = __importDefault(require("path"));
 class App {
-    private app: Application;
-    private http: http.Server;
-    private io: Server;
-    private usersOnline: { [key: string]: string } = {};
-
     constructor() {
-        this.app = express();
-        this.http = http.createServer(this.app);
-        this.io = new Server(this.http);
-        this.io = new Server(this.http, {
+        this.usersOnline = {};
+        this.app = (0, express_1.default)();
+        this.http = http_1.default.createServer(this.app);
+        this.io = new socket_io_1.Server(this.http);
+        this.io = new socket_io_1.Server(this.http, {
             maxHttpBufferSize: 10e6 // 10MB
         });
-
         this.listenSocket();
         this.setupRoutes();
     }
@@ -27,70 +26,60 @@ class App {
     listenSocket() {
         this.io.on('connection', (socket) => {
             console.log('user connected => ', socket.id);
-
             // Quando um usuário entra no chat
             socket.on('userJoined', (userName) => {
                 this.usersOnline[socket.id] = userName;
                 this.io.emit('updateUsers', Object.values(this.usersOnline));
             });
-
             // Quando um usuário envia uma mensagem
             socket.on('message', (msg) => {
                 console.log(`Received message from ${msg.name}: ${msg.content}`);
-                socket.emit('message', { ...msg, fromUser: true });
-                socket.broadcast.emit('message', { ...msg, fromUser: false });
+                socket.emit('message', Object.assign(Object.assign({}, msg), { fromUser: true }));
+                socket.broadcast.emit('message', Object.assign(Object.assign({}, msg), { fromUser: false }));
             });
-
             // Quando um usuário está digitando
             socket.on('typing', (name) => {
                 socket.broadcast.emit('typing', name);
             });
-
             // Quando um usuário para de digitar
             socket.on('stopTyping', () => {
                 socket.broadcast.emit('stopTyping');
             });
-
             // Quando um usuário envia uma imagem
             socket.on('image', (data) => {
                 console.log(`Imagem recebida de ${data.name}`);
                 socket.emit('image', data);
                 socket.broadcast.emit('image', data);
             });
-
             // Quando um usuário desconecta
             socket.on('disconnect', () => {
                 console.log('Usuário desconectado:', socket.id);
                 delete this.usersOnline[socket.id];
                 this.io.emit('updateUsers', Object.values(this.usersOnline));
             });
-
             // WebRTC - Oferta de conexão
             socket.on("offer", (data) => {
                 // Envia a oferta para todos os outros usuários (pode adaptar para direcionar a um usuário específico)
                 socket.broadcast.emit("offer", data);
             });
-
             // WebRTC - Resposta à oferta
             socket.on("answer", (data) => {
                 // Retorna a resposta para quem fez a oferta
                 socket.broadcast.emit("answer", data);
             });
-
             // WebRTC - Troca de ICE Candidates
             socket.on("candidate", (data) => {
                 // Compartilha os candidatos com todos os outros sockets
                 socket.broadcast.emit("candidate", data);
             });
         });
-
     }
     setupRoutes() {
-        this.app.use(express.static(path.resolve(__dirname, '../../frontend')));
+        this.app.use(express_1.default.static(path_1.default.resolve(__dirname, '../../frontend')));
         this.app.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+            res.sendFile(path_1.default.join(__dirname, "../../frontend/index.html"));
         });
     }
 }
-const app = new App()
-app.listenServer()
+const app = new App();
+app.listenServer();
